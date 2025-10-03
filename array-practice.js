9c1a6cec905866a16dea3903d37f38973073547b
Andrea Thompson <athompson314@yahoo.com>
2025-10-03 18:58:17 -0500

    practice 1  with arrays

--- array-practice.js
+++ array-practice.js
@@ -1,1 +1,13 @@
+//create an array with 5 grocery items
+let groceryList = ["milk", "bread", "apples", "butter", "carrots"];
+//create an array with 3 prices
+let prices = [13.99, 9.99, 11.79];
 
+//print grocery array
+console.log(groceryList);
+//print price array
+console.log(prices);
+//print first three items in grocery list
+console.log(groceryList.slice(0,3));
+//print first and third price
+console.log(prices[0], prices[2]);
